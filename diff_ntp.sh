f4211413d241bb260cfe1d4f78fa0224855678b0
JWhyFR <54511523+JWhyFR@users.noreply.github.com>
2025-09-03 15:43:26 +0200

    Update diff_ntp.sh

--- diff_ntp.sh
+++ diff_ntp.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+LOG=""
+function log() {
+  LOG+="$1"$'\n'
+}
+
+log ""
+log ""
+log "======================================="
+
+
 # Si jamais ntpdate n'était pas dispo...
 apt-get update 1>/dev/null 2>&1 && \
  apt-get install -y --no-install-recommends --no-install-suggests curl ntpsec-ntpdate 1>/dev/null 2>&1 
@@ -10,17 +21,17 @@ function show_diff() {
   local label="$1"
   local diff="$2"
   if [ ${diff#-} -gt 10 ]; then
-    echo "$label : décalage de ${diff}s par rapport à l'heure système."
+    log "$label : décalage de ${diff}s par rapport à l'heure système."
   else
-    echo "$label : synchronisé (écart de ${diff}s)."
+    log "$label : synchronisé (écart de ${diff}s)."
   fi
 }
 
-echo "Vérification de l'heure système..."
+log "Vérification de l'heure système..."
 
 # 1. Heure système locale
 LOCAL_TS=$(date -u +%s)
-echo "Heure système UTC : $LOCAL_TS"
+log "Heure système UTC : $LOCAL_TS"
 
 # 2. Heure via API publique (worldclockapi.com)
 API_TIME=$(curl -s  http://worldtimeapi.org/api/timezone/Etc/UTC | grep -oP '"unixtime":\K\d+')
@@ -28,7 +39,7 @@ if [ -n "$API_TIME" ]; then
   DIFF_API=$((LOCAL_TS - API_TIME))
   show_diff "API publique" "$DIFF_API"
 else
-  echo "API publique : Impossible de récupérer l'heure."
+  log "API publique : Impossible de récupérer l'heure."
 fi
 
 # 3. Heure via NTP (time.google.com)
@@ -38,5 +49,12 @@ if [[ "$OFFSET" =~ ^[-+]?[0-9.]+$ ]]; then
   OFFSET_INT=$(printf "%.0f" "$OFFSET")
   show_diff "Serveur NTP" "$OFFSET_INT"
 else
-  echo "Serveur NTP : Impossible de récupérer l'heure."
+  log "Serveur NTP : Impossible de récupérer l'heure."
 fi
+
+log "======================================="
+log ""
+log ""
+
+echo "$LOG"
+

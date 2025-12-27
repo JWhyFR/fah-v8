15e3020d21da5bda8f76f6963750a68bd9529d85
JWhyFR <54511523+JWhyFR@users.noreply.github.com>
2025-12-27 10:08:14 +0100

    Update Folding@home client download URL to latest

--- fah-onstart.sh
+++ fah-onstart.sh
@@ -53,7 +53,7 @@ echo "**** install runtime packages : lufah ****" && \
    
 echo "**** install foldingathome ****" && \
   mkdir /var/log/fah-client && \
-  download_url="https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.4.9-64bit-release.tar.bz2" && \
+  download_url="https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/latest.tar.bz2" && \
   curl -o \
     /tmp/fah.tar.bz2 -L \
     ${download_url} && \
